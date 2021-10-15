4097d35b7857a90926059ff74e6a60aba5e02383
Gontjarow <niko.gontjarow@gmail.com>
2021-10-15 05:56:19 +0300

    Added server config

---
+++ server/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define ("BOOKS_URI", "https://openlibrary.org/");
+define ("BOOKS_API", "apikey");
+
+define ("MOVIES_URI", "http://www.omdbapi.com/");
+define ("MOVIES_API", "apikey");
+
+?>
