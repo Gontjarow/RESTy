245bb84b180d702987313de4be22c14700af141f
Gontjarow <niko.gontjarow@gmail.com>
2021-10-15 05:55:34 +0300

    Init + index page

---
+++ client/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Search Engine (tm)</title>
+</head>
+<body>
+
+<form action="client.php" method="post">
+
+    <label>Search for</label>
+
+    <div>
+        <div style="display:inline-block">
+            <input name="Category" type="radio" id="Books" value="Books">
+            <label>Books</label>
+        </div>
+        <div style="display:inline-block">
+            <input name="Category" type="radio" id="Movies" value="Movies" checked="checked">
+            <label>Movies</label>
+        </div>
+    </div>
+
+    <input type="text" pattern="[a-zA-Z0-9 :_]+" placeholder="Search..." class="form-input" name="txt" id="txt">
+
+</div>
+
+</body>
+</html>
