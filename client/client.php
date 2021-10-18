6cc8349b48cda6ff6c7aa4bb9214811efcd545b5
Gontjarow <niko.gontjarow@gmail.com>
2021-10-18 17:11:00 +0300

    Next up: Require valid JWT for endpoint.

--- client/client.php
+++ client/client.php
@@ -6,7 +6,11 @@ require_once("../server/endpoint.php");
 $uri = explode("/", $_SERVER["PATH_INFO"]);
 parse_str($_SERVER["QUERY_STRING"], $query);
 
+// I can't tell if this kinda defeats the purpose of being a client.
 $endpoint = new Endpoint();
+
+// todo: Request JWT and include it.
+// Endpoint will require/verify it.
 
 $endpoint->{$uri[1]}($query);
 
