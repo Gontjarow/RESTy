e5620dd8a064989dbcc814cb5662fe8db606bd90
Gontjarow <niko.gontjarow@gmail.com>
2021-10-19 04:24:23 +0300

    Rewrote curl.php with args.

--- client/curl.php
+++ client/curl.php
@@ -3,38 +3,44 @@
 $jwt = "header.body.signature";
 $url = null;
 
-var_dump($argc);
-var_dump($argv);
-
-if ($argc > 1)
+if ($argc > 1 && $argv[1] != "auth")
 {
-    if ($argv[1] == "auth")
-        $url = "http://localhost:8000/client.php/auth";
-    else if ($argv[1] == "book")
+    if ($argv[1] == "book")
+    {
+        $argv[2] = $argv[2] ?? "";
         $url = "http://localhost:8000/client.php/getBooks?ISBN={$argv[2]}";
+    }
     else if ($argv[1] == "movie")
+    {
+        $argv[2] = $argv[2] ?? "";
+        $argv[3] = $argv[3] ?? "";
+        $argv[4] = $argv[4] ?? "";
         $url = "http://localhost:8000/client.php/getMovies?Title={$argv[2]}&Year={$argv[3]}&Plot={$argv[4]}";
+    }
+    else
+    {
+        echo "commands: auth, book (ISBN), movie (Title) [Year] [Plot]\n";
+    }
 }
 else
 {
     $url = "http://localhost:8000/client.php/auth";
 }
 
-var_dump($url);
+if (!empty($url))
+{
+    $handle = curl_init($url);
+    $headers = array(
+        "Accept: application/json",
+        "Authorization: Bearer {$jwt}",
+     );
 
-$handle = curl_init($url);
-$headers = array(
-    "Accept: application/json",
-    "Authorization: Bearer {$jwt}",
- );
+    curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
 
-curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
-
-$data = curl_exec($handle);
-curl_close($handle);
-var_dump($data);
+    $data = curl_exec($handle);
+    curl_close($handle);
+    echo $data."\n";
+}
 
 ?>
